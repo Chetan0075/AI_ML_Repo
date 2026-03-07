0c140a0ee5eb5c438a5f4d93e16b3a5335053315
Chetan Suraswal <151898636+Chetan0075@users.noreply.github.com>
2026-03-07 12:37:56 +0530

    Add model accuracy evaluation for house purchase prediction
    This script predicts house purchasing ability based on salary using Logistic Regression and KNN, and calculates their accuracy.

---
+++ python_codes/accuracy_of_models.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+import warnings
+warnings.filterwarnings("ignore")
+from sklearn.metrics import accuracy_score
+
+
+salary = int(input("Enter Your Salary : "))
+df = pd.read_csv("AI_ML_Workshop/User_Data.csv")
+# print(df)
+# df.info()
+
+x = df.iloc[:,[3]] #salary
+y = df.iloc[:,[4]] #house
+# print(x,y)
+# left side of (comma) means row number and right of (comma) means column number 
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size= 0.2)
+
+
+
+
+print("---------------------Using Logistical Regression-------------------")
+lr = LogisticRegression()
+lr.fit(x_train,y_train)
+y_pred = lr.predict([[salary]])
+
+if y_pred == 0 :
+    print("You Cannot Purchase House Your Salary is Low")
+
+else :
+    print("You Can purchase House You Have Enough Salary")
+
+print("---------------------Accuracy of Logistical Regression -------------------")
+y_pred2 = lr.predict(x_test)
+score = accuracy_score(y_test,y_pred2)
+print(score)
+
+
+
+
+
+print("---------------------Using KNN------------------------------------")
+knn = KNeighborsClassifier()
+knn.fit(x_train,y_train)
+knn_pred = knn.predict([[salary]])
+
+if knn_pred == 0 :
+    print("You Cannot Purchase House Your Salary is Low")
+
+else :
+    print("You Can purchase House You Have Enough Salary")
+
+print("---------------------Accuracy of KNN-------------------")
+knn_pred = lr.predict(x_test)
+score2 = accuracy_score(y_test,y_pred2)
+print(score2)    
