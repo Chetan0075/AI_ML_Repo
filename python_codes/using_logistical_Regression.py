3b00f618dc88f0dc1ac32d3b166db6301a910d31
Chetan Suraswal <151898636+Chetan0075@users.noreply.github.com>
2026-03-07 10:52:53 +0530

    Add logistic regression model for house prediction

---
+++ python_codes/using_logistical_Regression.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+salary = int(input("Enter Your Salary : "))
+
+df = pd.read_csv("AI_ML_Workshop/User_Data.csv")
+print(df)
+# df.info()
+
+
+x = df.iloc[:,[3]] #salary
+y = df.iloc[:,[4]] #house
+# print(x,y)
+# left side of (comma) means row number and right of (comma) means column number 
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size= 0.2)
+
+lr = LogisticRegression()
+lr.fit(x_train,y_train)
+y_pred = lr.predict([[salary]])
+print("Your Predicted House is : ",y_pred)
+
